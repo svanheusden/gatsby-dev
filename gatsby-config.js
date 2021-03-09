4a1d0efb47914223da2d78bb9c668b2f2f1efa6f
svanheusden <velocio@gmail.com>
2021-03-09 16:24:38 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Site title`,
+    description: `Lorum ipsum SEO description`,
+    author: `svanheusden`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
